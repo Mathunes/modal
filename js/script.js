9968f7498346b4d453df72a28e79a931d52e2886
mathunes <matheusantunes720@gmail.com>
2020-01-08 14:39:29 -0300

    Desativacao do modal com o click

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ const showModal = () => {
     modal.style.top = "0px";
 }
 
+const hideModal = () => {
+    const modal = document.querySelector(".modal");
+    modal.style.top = "-10000px";
+
+    const containerModal = document.querySelector(".container-modal");
+    containerModal.style.opacity = "0";
+    containerModal.style.zIndex = "-1";
+}
+
 document.querySelector(".container .modal-btn").addEventListener("click", () => {
     showModal();
+});
+
+document.querySelector(".container-modal .modal .modal-btn-group .modal-btn").addEventListener("click", () => {
+    hideModal();
 });
